042b35c36ddebc9ade674b04446931ac8c1a949e
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-05-05 17:10:28 +0300

    Initial commit

---
+++ src/main/java/shchuko/git_fast_reword/GitFastReword.java
@@ -0,0 +1,7 @@
+package shchuko.git_fast_reword;
+
+import org.eclipse.jgit.api.Git;
+
+public class GitFastReword {
+
+}
---
+++ src/main/java/shchuko/git_fast_reword/Main.java
@@ -0,0 +1,7 @@
+package shchuko.git_fast_reword;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
